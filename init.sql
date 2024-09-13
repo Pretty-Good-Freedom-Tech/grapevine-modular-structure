169f7037817edb059f81bbe720586983e4a187ff
PrettyGoodFreedomTech <pretty.good.freedom.tech@gmail.com>
2024-09-13 19:11:30 -0400

    Update init.sql

--- init.sql
+++ init.sql
@@ -1,32 +1,100 @@
--- SQLITE
+/*
+SQLITE
 
--- DATABASE: GrapevineCalculationEngine.db
+MAIN DATABASE: GrapevineCalculationEngine.db
 
--- ONE SET OF TABLES PER ENGINE:
--- table1: Raw Data types (nostr, AI, Amazon, etc)
--- table2: raw data Engine for each type 
--- table3: Interp Engines for each raw data type
--- table4: available protocols for each Interp Engine
--- table5: users (customers)
+ONE SET OF TABLES PER ENGINE:
 
--- ONE SET OF BELOW TABLES FOR EACH USER (perhaps make a new database for each user??)
--- DATABASE: <pk_Alice>_GCE.db
--- userTable1: GrapeRankScorecards tables (G)
--- userTable2: ? GrapeRankRatings tables (R)
--- userTable3: various param tables
--- userTable4: Grapevine Worldview tables
+table1: Raw Data types (nostr, AI, Amazon, etc) rawDataTypes -- rawDataTypes
+table2: Interp Engines for each raw data type -- interpretationEngines
+table3: raw data Engine for each type  -- rawDataEngines
+table4: available interpretation protocols for each Interp Engine -- interpretationProtocols
+table5: users (customers) -- users
+
+for each new user, make a new database:
+SINGLE USER DATABASE: <pk_Alice>_GCE.db
+
+userTable1: GrapeRank Scorecards (G) -- grapeRankScorecards
+userTable2: GrapeRank Ratings (R) -- grapeRankRatings
+userTable3: standard Grapevine Calculation Parameters (attenFactor, rigor, defaults (?), etc) -- grapevineCalculationParams
+userTable4: protocol-specific parameters (score, confidence, etc) -- protocolParams
+userTable4: worldviews Grapevine Worldview tables
+
+*/
+
+----------------- MAIN DATABASE, 5 tables
 
 -- Create main database
-
 GrapevineCalculationEngine.db
 
--- create table1 and insert some rows
+-- table1
 CREATE TABLE rawDataTypes(
   ID INT PRIMARY KEY NOT NULL,
+  SLUG TEXT NOT NULL,
   NAME TEXT NOT NULL,
   DESCRIPTION TEXT,
 );
 
-INSERT INTO rawDataTypes [(name, description)] VALUES ("nostr relay", "lorem ipsum");
-INSERT INTO rawDataTypes [(name, description)] VALUES ("ChatGPT", "lorem ipsum");
-INSERT INTO rawDataTypes [(name, description)] VALUES ("Amazon", "lorem ipsum");
+INSERT INTO rawDataTypes [(slug, name, description)] VALUES ("nostrRelay", "nostr relay", "lorem ipsum"); -- id: 0
+INSERT INTO rawDataTypes [(slug, name, description)] VALUES ("chatGPT", "Chat GPT", "lorem ipsum"); -- id: 1
+INSERT INTO rawDataTypes [(slug, name, description)] VALUES ("Amazon", "Amazon", "lorem ipsum"); -- id: 2
+
+-- table2
+CREATE TABLE interpretationEngines(
+  ID INT PRIMARY KEY NOT NULL,
+  rawDataTypeID INT NOT NULL,
+  slug TEXT NOT NULL,
+  name TEXT NOT NULL,
+  description TEXT,
+);
+
+INSERT INTO interpretationEngines [(rawDataTypeID, slug, name)] VALUES (0, "ManiMeNostrInterpEngine", "ManiMe's Awesome Nostr Interpretation Engine");
+
+/* table3 MIGHT BELONG INSIDE THE INTERPRETATION ENGINE, BC IT'S STRUCTURE WILL DEPEND ON THE RAW DATA TYPE. */
+-- table3
+CREATE TABLE rawDataEngines_nostrRelays(
+  ID INT PRIMARY KEY NOT NULL,
+  rawDataTypeID INT NOT NULL,
+  slug TEXT NOT NULL,
+  name TEXT NOT NULL,
+  url TEXT NOT NULL,
+);
+
+INSERT INTO rawDataEngines_nostrRelays [(rawDataTypeID, name)] VALUES (0, "cloudfodder's Awesome Relay", "wss://brainstorm.nostr1.com");
+
+-- table4
+CREATE TABLE interpretationProtocols(
+  ID INT PRIMARY KEY NOT NULL,
+);
+
+-- table5
+CREATE TABLE users(
+  ID INT PRIMARY KEY NOT NULL,
+);
+
+------------- ONE DATABASE PER USER, 4 tables
+
+-- Create user-specific database
+<pk_Alice>_GCE.db;
+
+-- userTable1
+CREATE TABLE grapeRankScorecards(
+  ID INT PRIMARY KEY NOT NULL,
+);
+
+-- userTable2
+CREATE TABLE grapeRankRatings(
+  ID INT PRIMARY KEY NOT NULL,
+);
+
+-- userTable3
+CREATE TABLE grapevineCalculationParams(
+  ID INT PRIMARY KEY NOT NULL,
+);
+
+-- userTable4
+CREATE TABLE protocolParams(
+  ID INT PRIMARY KEY NOT NULL,
+);
+
+
