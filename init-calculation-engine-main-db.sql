89f5a71e644c30c5f0198ed1e197f275301474bd
PrettyGoodFreedomTech <pretty.good.freedom.tech@gmail.com>
2024-09-14 13:03:09 -0400

    Update init-calculation-engine-main-db.sql

--- init-calculation-engine-main-db.sql
+++ init-calculation-engine-main-db.sql
@@ -1,65 +1,50 @@
-/*
-SQLITE
 
-MAIN DATABASE: GrapevineCalculationEngine.db
+-- Create core database
+GrapevineCalculationEngine_core.db
 
-ONE SET OF TABLES PER ENGINE:
+-- coreTable1
+CREATE TABLE users(
+  ID INT PRIMARY KEY NOT NULL,
+  pubkey TEXT UNIQUE NOT NULL,
+  whenSignedUp TIMESTAMP NOT NULL,
+  subscriptionPlan TEXT NOT NULL, -- will need to flesh out later what this means
+);
 
-table1: Raw Data Source Categories (e.g. nostr relay, AI, Amazon, etc) rawDataTypes -- rawDataSourceCategories
-table2: Raw Data Sources (e.g. wss://brainstorm.nostr1,com)  -- rawDataSources
-table3: Interp Engines for each raw data type -- interpretationEngines
-table4: available interpretation protocols for each Interp Engine -- interpretationProtocols
-table5: users (customers) -- users
-
-*/
-
------------------ MAIN DATABASE, 5 tables
-
--- Create main database
-GrapevineCalculationEngine.db
-
--- table1
+-- coreTable2
 CREATE TABLE rawDataSourceCategories(
   ID INT PRIMARY KEY NOT NULL,
-  SLUG TEXT NOT NULL,
+  SLUG TEXT UNIQUE NOT NULL,
   NAME TEXT NOT NULL,
-  -- ?? rawDataSourceTable TEXT NOT NULL,
   DESCRIPTION TEXT,
 );
 
-INSERT INTO rawDataSourceCategories [(slug, name, rawDataSourceTable, description)] VALUES ("nostrRelay", "nostr relay", "rawDataSources_nostrRelays", "lorem ipsum"); -- id: 0
-INSERT INTO rawDataSourceCategories [(slug, name, rawDataSourceTable, description)] VALUES ("chatGPT", "Chat GPT", "rawDataSources_chatGPT", "lorem ipsum"); -- id: 1
-INSERT INTO rawDataSourceCategories [(slug, name, rawDataSourceTable, description)] VALUES ("Amazon", "Amazon", "rawDataSources_Amazon", "lorem ipsum"); -- id: 2
+INSERT INTO rawDataSourceCategories [(slug, name)] VALUES ("nostr", "nostr relays"); -- id: 0
+INSERT INTO rawDataSourceCategories [(slug, name)] VALUES ("chatGPT", "Chat GPT"); -- id: 1
+INSERT INTO rawDataSourceCategories [(slug, name)] VALUES ("Amazon", "Amazon"); -- id: 2
 
-/* table2 MIGHT BELONG INSIDE THE INTERPRETATION ENGINE, BC IT'S STRUCTURE WILL DEPEND ON THE RAW DATA TYPE. */
--- table2
-CREATE TABLE rawDataSources_nostrRelays(
+-- coreTable3
+CREATE TABLE rawDataSources(
   ID INT PRIMARY KEY NOT NULL,
-  -- rawDataSourceCategoryID INT NOT NULL,
-  slug TEXT NOT NULL,
+  slug TEXT UNIQUE NOT NULL,
   name TEXT NOT NULL,
-  url TEXT NOT NULL,
-  supportedProtocols TEXT, -- a stringified array of protocolSlugs
+  rawDataSourceCategorySlug TEXT NOT NULL, -- alternate: rawDataSourceCategoryID INT NOT NULL,
 );
-/*
-May also want other columns like: uptime or other performance measures, free vs paid, etc)
-*/
 
-INSERT INTO rawDataSources_nostrRelays [(name, url)] VALUES ("cloudfodder's Awesome Grapevine Relay", "wss://brainstorm.nostr1.com");
+INSERT INTO rawDataSources [(slug, name, rawDataSourceCategorySlug)] VALUES ("brainstormNostrRelay", "the Awesome Brainstorm nostr relay", "nostr" ); -- id: 0
 
--- table3
+-- coreTable4
 CREATE TABLE interpretationEngines(
   ID INT PRIMARY KEY NOT NULL,
-  rawDataSourceCategories INT NOT NULL,
-  slug TEXT NOT NULL,
+  slug TEXT UNIQUE NOT NULL,
   name TEXT NOT NULL,
   description TEXT,
-  supportedProtocols TEXT NOT NULL,
+  rawDataSourceCategorySlug INT NOT NULL, -- alternate: rawDataSourceCategoryID
+  aSupportedInterpretationProtocols TEXT NOT NULL, -- a stringified array of protocolSlugs
 );
 
-INSERT INTO interpretationEngines [(rawDataSourceCategories, slug, name)] VALUES (0, "ManiMeNostrInterpEngine", "ManiMe's Awesome Nostr Interpretation Engine");
+INSERT INTO interpretationEngines [(slug, name, rawDataSourceCategorySlug )] VALUES ("BrainstormNostrInterpEngine", "Brainstorm's Awesome Nostr Interpretation Engine", "nostr" );
 
--- table4a
+-- coreTable5
 CREATE TABLE interpretationProtocols(
   ID INT PRIMARY KEY NOT NULL,
   rawDataSourceCategories INT NOT NULL,
@@ -73,6 +58,26 @@ INSERT INTO interpretationProtocols [(rawDataSourceCategories, name)] VALUES (0,
 INSERT INTO interpretationProtocols [(rawDataSourceCategories, name)] VALUES (1, "Mutes Interpretation"); -- params required fields: score, confidence
 INSERT INTO interpretationProtocols [(rawDataSourceCategories, name)] VALUES (2, "Standard Reports Interpretation"); -- params required fields: score, confidence (all reportTypes are treated the same)
 INSERT INTO interpretationProtocols [(rawDataSourceCategories, name)] VALUES (3, "Expanded Reports Interpretation"); -- params score & confidence; can vary according to reportType
+
+
+
+/***************
+initialization code for rawDataSourceCategory = nostrRelays:
+***************/
+
+
+-- coreTable3_nostrRelays
+CREATE TABLE rawDataSources_nostrRelays(
+  ID INT PRIMARY KEY NOT NULL,
+  rawDataSourceID INT NOT NULL, -- points to coreTable3
+  url TEXT NOT NULL,
+);
+/*
+May also want other columns like: uptime or other performance measures, free vs paid, etc)
+*/
+
+INSERT INTO rawDataSources_nostrRelays [(name, url, aSupportedInterpretationProtocols)] VALUES ("cloudfodder's Awesome Grapevine Relay", "wss://brainstorm.nostr1.com", "[ 'basicFollow', 'basicMute', 'basicReport' ]");
+
 
 -- table4b
 CREATE TABLE defaultInterpretationProtocolSolutions(
@@ -103,10 +108,5 @@ CREATE TABLE userInterpretationProtocolSolutions(
 INSERT INTO userInterpretationProtocolSolutions [(interpretationProtocolId, userId, name)] VALUES (0, AlicdId, "My Usual Brainstorm Follows Interpretation"); -- params: score = 1, confidence = 0.05
 INSERT INTO userInterpretationProtocolSolutions [(interpretationProtocolId, userId, name)] VALUES (0, AlicdId, "My Alternate Brainstorm Follows Interpretation"); -- params: score = 1, confidence = 0.08
 
--- table5
-CREATE TABLE users(
-  ID INT PRIMARY KEY NOT NULL,
-  pubkey TEXT NOT NULL,
-);
 
 
