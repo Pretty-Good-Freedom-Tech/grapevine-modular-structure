59c579a3d58c0d6b95ec67497f89c6ff2430a501
PrettyGoodFreedomTech <pretty.good.freedom.tech@gmail.com>
2024-09-23 15:56:28 -0400

    Update database-initialization-core.sql

--- interpretation-engine/nostr/database-initialization-core.sql
+++ interpretation-engine/nostr/database-initialization-core.sql
@@ -2,7 +2,7 @@
 Initialization of the core database for a Nostr Interpretation Engine
 
 coreTable1: interpretationProtocols
-
+Note that coreTable1 (of the interpretation engine) and coreTable4_nostr (of the calculation engine) are very similar, and that the slugs must match exactly. (Maybe replace slug with "universalId" which is also a string, and may be a slug, but could also be a non-slug string e.g. a nostr event id, naddr, or some other hash?)
 */
 
 -- Create core database
