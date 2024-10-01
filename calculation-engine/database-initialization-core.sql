abb8e21c162eac25a172b5102a2bb171ed26d074
PrettyGoodFreedomTech <pretty.good.freedom.tech@gmail.com>
2024-10-01 16:51:26 -0400

    Update database-initialization-core.sql

--- calculation-engine/database-initialization-core.sql
+++ calculation-engine/database-initialization-core.sql
@@ -1,16 +1,21 @@
 
+-- code written for postgres, testing on vercel 
+/*
+DROP TABLE table
+*/
+
 -- Create core database
 GrapevineCalculationEngine_core.db
 
 -- coreTable1
 CREATE TABLE IF NOT EXISTS users(
-  ID INT SERIAL PRIMARY KEY,
+  ID SERIAL PRIMARY KEY,
   pubkey VARCHAR(255) UNIQUE NOT NULL
 );
 
 -- coreTable2
 CREATE TABLE IF NOT EXISTS rawDataSourceCategories(
-  ID INT SERIAL PRIMARY KEY,
+  ID SERIAL PRIMARY KEY,
   SLUG TEXT UNIQUE NOT NULL,
   NAME TEXT,
   DESCRIPTION TEXT
@@ -22,88 +27,77 @@ INSERT INTO rawDataSourceCategories (slug, name) VALUES ('chatGPT', 'Chat GPT');
 INSERT INTO rawDataSourceCategories (slug, name) VALUES ('AI', 'AI');
 
 -- coreTable3
+/*
+This table maay not be needed; it may be left up to the interpretation engine to select individual data sources (eg specific relays). Alternatively, the interpretationProtocol could have 
+the OPTION to request particular data sources.
+*/
 CREATE TABLE IF NOT EXISTS rawDataSources(
-  ID INT PRIMARY KEY NOT NULL,
+  ID SERIAL PRIMARY KEY,
   slug TEXT UNIQUE NOT NULL,
   name TEXT NOT NULL,
   description TEXT,
-  rawDataSourceCategorySlug TEXT NOT NULL, -- points to coreTable2, rawDataSourceCategories.slug (alternate: rawDataSourceCategoryID INT NOT NULL, points to rawDataSourceCategories.id)
+  rawDataSourceCategorySlug TEXT NOT NULL -- points to coreTable2, rawDataSourceCategories.slug (alternate: rawDataSourceCategoryID INT NOT NULL, points to rawDataSourceCategories.id)
 );
 
-INSERT INTO rawDataSources [(slug, name, rawDataSourceCategorySlug)] VALUES ("brainstormNostrRelay", "The Awesome Brainstorm Nostr Relay", "nostr" );
+INSERT INTO rawDataSources (slug, name, rawDataSourceCategorySlug) VALUES ('brainstormNostrRelay', 'The Awesome Brainstorm Nostr Relay', 'nostr' );
 
 -- coreTable4
 CREATE TABLE IF NOT EXISTS interpretationEngines(
-  ID INT PRIMARY KEY NOT NULL,
+  ID SERIAL PRIMARY KEY,
   slug TEXT UNIQUE NOT NULL,
   name TEXT,
   title TEXT,
   description TEXT,  
-  rawDataSourceCategorySlug TEXT NOT NULL, -- points to coreTable2, rawDataSourceCategories.slug (alternate: rawDataSourceCategoryID INT NOT NULL, points to rawDataSourceCategories.id)
-  
-  -- DEPRECATED: moved to coreTable4_j
-  aSupportedInterpretationProtocolSlugs TEXT NOT NULL, -- a stringified array of interpretationProtocolSlugs
-  -- DEPRECATED: changed to single value
-  aSupportedRawDataSourceCategorySlugs TEXT NOT NULL, -- a stringified array of rawDataSourceCategorySlugs pointing to coreTable2, rawDataSourceCategories.slug (alternate: id instead of slug)
+  aSupportedRawDataSourceCategorySlugs JSONB NOT NULL -- points to coreTable2, rawDataSourceCategories.slug (alternate: aSupportedRawDataSourceCategoryIDs INT NOT NULL, points to rawDataSourceCategories.id)
 );
 
-INSERT INTO interpretationEngines [(slug, name, rawDataSourceCategorySlug )] VALUES ("BrainstormNostrInterpEngine", "The Awesome Brainstorm Nostr Interpretation Engine", "nostr" );
+INSERT INTO interpretationEngines (slug, name, aSupportedRawDataSourceCategorySlugs ) VALUES ('BrainstormNostrInterpEngine', 'The Awesome Brainstorm Nostr Interpretation Engine', '{ "aSlugs": [ "nostr" ] }' );
 
 -- coreTable5
 CREATE TABLE IF NOT EXISTS interpretationProtocols(
-  ID INT PRIMARY KEY NOT NULL,
+  ID SERIAL PRIMARY KEY,
   slug TEXT UNIQUE NOT NULL,
-  name TEXT NOT NULL,
+  name TEXT,
   description TEXT,
-  rawDataSourceCategorySlug TEXT NOT NULL, -- points to coreTable2, rawDataSourceCategories.slug (alternate: rawDataSourceCategoryID INT NOT NULL, points to rawDataSourceCategories.id)
-
-  ---?????????:
-  parametersSchema TEXT NOT NULL, -- stringified JSON schema (json-schema.org) template for all required and optional parameters, which may be very different for each protocol. This may or may not include default values.
-  
-  -- ALTERNATE to parametersSchema:
-  parametersSchemaNaddr TEXT NOT NULL, -- naddr to an event with the JSON Schema, managed by Brainstorm. Advantage: multiple (competing) services can point to this naddr and ensure compatibility with the wider community
-  ----??????
-
-  
-  -- DEPRECATED: 
-  aSupportedRawDataSourceCategorySlugs TEXT NOT NULL, -- a stringified array of rawDataSourceCategorySlugs pointing to coreTable2, rawDataSourceCategories.slug (alternate: id instead of slug)
+  rawDataSourceCategorySlug TEXT NOT NULL -- points to coreTable2, rawDataSourceCategories.slug (alternate: rawDataSourceCategoryID INT NOT NULL, points to rawDataSourceCategories.id)
 );
 
--- universal (rawDataSourceCategorySlug is null or "all" or "GrapeRank")
--- DEPRECATING INSERT INTO interpretationProtocols [(slug, name, rawDataSourceCategorySlug )] VALUES ("grapeRank", "GrapeRank", "grapeRank" );
+-- universal (rawDataSourceCategorySlug is null or 'all' or 'GrapeRank')
+-- DEPRECATING INSERT INTO interpretationProtocols slug, name, rawDataSourceCategorySlug ) VALUES ('grapeRank', 'GrapeRank', 'grapeRank' );
 
 -- specific to rawDataSourceCategorySlug
-INSERT INTO interpretationProtocols [(slug, name, rawDataSourceCategorySlug )] VALUES ("basicFollowsInterpretation", "the Standard Follows Interpretation", "nostr" );
-INSERT INTO interpretationProtocols [(slug, name, rawDataSourceCategorySlug )] VALUES ("basicMutesInterpretation", "the Standard Mutes Interpretation", "nostr" );
-INSERT INTO interpretationProtocols [(slug, name, rawDataSourceCategorySlug )] VALUES ("basicReportsInterpretation", "the Standard Reports Interpretation", "nostr" );
-INSERT INTO interpretationProtocols [(slug, name, rawDataSourceCategorySlug )] VALUES ("expandedReportsInterpretation", "the Expanded Reports Interpretation", "nostr" );
+INSERT INTO interpretationProtocols (slug, name, rawDataSourceCategorySlug ) VALUES ('basicFollowsInterpretation', 'the Standard Follows Interpretation', 'nostr' );
+INSERT INTO interpretationProtocols (slug, name, rawDataSourceCategorySlug ) VALUES ('basicMutesInterpretation', 'the Standard Mutes Interpretation', 'nostr' );
+INSERT INTO interpretationProtocols (slug, name, rawDataSourceCategorySlug ) VALUES ('basicReportsInterpretation', 'the Standard Reports Interpretation', 'nostr' );
+INSERT INTO interpretationProtocols (slug, name, rawDataSourceCategorySlug ) VALUES ('expandedReportsInterpretation', 'the Expanded Reports Interpretation', 'nostr' );
+INSERT INTO interpretationProtocols (slug, name, rawDataSourceCategorySlug ) VALUES ('standardGrapevineNetworkInterpretation', 'the Standard Grapevine Network Interpretation', 'nostr' ); -- this is a combo of follows, mutes, and reports all in one
 
-INSERT INTO interpretationProtocols [(slug, name, rawDataSourceCategorySlug )] VALUES ("standardGrapevineNetworkInterpretation", "the Standard Grapevine Network Interpretation", "nostr" ); -- this is a combo of follows, mutes, and reports all in one
-
-INSERT INTO interpretationProtocols [(slug, name, rawDataSourceCategorySlug )] VALUES ("basicAmazonInterpretation", "Amazon Product Ratings Interpretation", "Amazon" );
+/*
+INSERT INTO interpretationProtocols (slug, name, rawDataSourceCategorySlug ) VALUES ('basicAmazonInterpretation', 'Amazon Product Ratings Interpretation', 'Amazon' );
+*/
 
 -- coreTable6
 CREATE TABLE IF NOT EXISTS grapeRankProtocols(
-  ID INT PRIMARY KEY NOT NULL,
+  ID SERIAL PRIMARY KEY,
   slug TEXT UNIQUE NOT NULL,
-  name TEXT NOT NULL,
+  name TEXT,
   description TEXT,
-  
-  -- nostr-specific columns
-  parametersJsonSchema TEXT NOT NULL, -- stringified JSON schema (json-schema.org) template for all required and optional parameters, which may be very different for each protocol. This may or may not include default values.
-  
-  -- ALTERNATE to parametersSchema:
-  parametersJsonSchemaNaddr TEXT NOT NULL, -- naddr to an event with the JSON Schema, managed by Brainstorm. Advantage: multiple (competing) services can point to this naddr and ensure compatibility with the wider community
+  parametersJsonSchema JSONB, -- stringified JSON schema (json-schema.org) template for all required and optional parameters, which may be very different for each protocol. This may or may not include default values.
+  parametersJsonSchemaNaddr TEXT -- naddr to an event with the JSON Schema, managed by Brainstorm. Advantage: multiple (competing) services can point to this naddr and ensure compatibility with the wider community
 );
 
-INSERT INTO grapeRankProtocols [(slug, parametersSchema )] VALUES ("basicGrapevineNetwork", "{ properties: { attenuation: { type: float, min: 0, max: 1, default: 0.8 }, rigor: { type: float, min: 0, max: 1, default: 0.25 }, defaultUserScore: { type: float, min: 0, max: 1, default: 0.0 }, defaultUserScoreConfidence: { type: float, min: 0, max: 1, default: 0.01} } }" );
-INSERT INTO grapeRankProtocols [(slug, parametersSchema )] VALUES ("basic5StarProductCalculation", "{ properties: { defaultProductScore: { type: float, min: 0, max: 5, default: 0.0 }, defaultProductScoreConfidence: { type: float, min: 0, max: 1, default: 0.05 } }}" );
+INSERT INTO grapeRankProtocols (slug, parametersJsonSchema ) VALUES ('basicGrapevineNetwork', '{ "properties": { "attenuation": { "type": "float", "min": 0, "max": 1, "default": 0.8 }, "rigor": { "type": "float", "min": 0, "max": 1, "default": 0.25 }, "defaultUserScore": { "type": "float", "min": 0, "max": 1, "default": 0.0 }, "defaultUserScoreConfidence": { "type": "float", "min": 0, "max": 1, "default": 0.01} } }' );
+INSERT INTO grapeRankProtocols (slug, parametersJsonSchema ) VALUES ('basic5StarProductCalculation', '{ "properties": { "defaultProductScore": { "type": "float", "min": 0, "max": 5, "default": 0.0 }, "defaultProductScoreConfidence": { "type": "float", "min": 0, "max": 1, "default": 0.05 } }}' );
+
+-- deprecating bc switching parametersJsonSchema from TEXT to JSONB
+-- INSERT INTO grapeRankProtocols (slug, parametersJsonSchema ) VALUES ('basicGrapevineNetwork', '{ properties: { attenuation: { type: float, min: 0, max: 1, default: 0.8 }, rigor: { type: float, min: 0, max: 1, default: 0.25 }, defaultUserScore: { type: float, min: 0, max: 1, default: 0.0 }, defaultUserScoreConfidence: { type: float, min: 0, max: 1, default: 0.01} } }' );
+-- INSERT INTO grapeRankProtocols (slug, parametersJsonSchema ) VALUES ('basic5StarProductCalculation', '{ properties: { defaultProductScore: { type: float, min: 0, max: 5, default: 0.0 }, defaultProductScoreConfidence: { type: float, min: 0, max: 1, default: 0.05 } }}' );
 
 -- coreTable7
 CREATE TABLE IF NOT EXISTS protocolParameterSelections(
-  ID INT PRIMARY KEY NOT NULL,
+  ID SERIAL PRIMARY KEY,
   userID ID UNIQUE NOT NULL,
-  protocolCategoryTableName TEXT NOT NULL, -- "interpretationProtocols" or "grapeRankProtocols" or (coreTable5 or coreTable6) 
+  protocolCategoryTableName TEXT NOT NULL, -- 'interpretationProtocols' or 'grapeRankProtocols' or (coreTable5 or coreTable6) 
   protocolSlug TEXT NOT NULL, [protocolCategoryTableName].slug
   
   selectedParameters TEXT NOT NULL, -- stringified JSON that contains the parameters with selected values
@@ -116,22 +110,22 @@ CREATE TABLE IF NOT EXISTS protocolParameterSelections(
 );
 
 -- defaults
-INSERT INTO protocolParameterSelections (userID, protocolCategoryTableName, protocolSlug, selectedParameters ) VALUES ("default", "interpretationProtocols", "basicFollowsInterpretation", "{ score: 1, confidence: 0.05 }" );
-INSERT INTO protocolParameterSelections (userID, protocolCategoryTableName, protocolSlug, selectedParameters ) VALUES ("default", "interpretationProtocols", "basicMutesInterpretation", "{ score: 0, confidence: 0.10 }" );
-INSERT INTO protocolParameterSelections (userID, protocolCategoryTableName, protocolSlug, selectedParameters ) VALUES ("default", "interpretationProtocols", "basicReportsInterpretation", "{ score: 0, confidence: 0.20 }" );
-INSERT INTO protocolParameterSelections (userID, protocolCategoryTableName, protocolSlug, selectedParameters ) VALUES ("default", "interpretationProtocols", "expandedReportsInterpretation", "{ reportTypesGroupA: { reportTypes: [ malware, illegal, spam, impersonation ], score: 1, confidence: 0.5 }, reportTypesGroupB: { reportTypes: [ profanity, nudity ], score: 1, confidence: 0.02 }, reportTypesGroupC: { reportTypes: [ other ], score: 1, confidence: 0.1 }, }" );
-INSERT INTO protocolParameterSelections (userID, protocolCategoryTableName, protocolSlug, selectedParameters ) VALUES ("default", "grapeRankProtocols", "basicGrapevineNetwork", "{ attenuation: 0.8, rigor: 0.25, defaultUserScore: 0, defaultUserScoreConfidence: 0.01 }" );
+INSERT INTO protocolParameterSelections (userID, protocolCategoryTableName, protocolSlug, selectedParameters ) VALUES ('default', 'interpretationProtocols', 'basicFollowsInterpretation', '{ score: 1, confidence: 0.05 }' );
+INSERT INTO protocolParameterSelections (userID, protocolCategoryTableName, protocolSlug, selectedParameters ) VALUES ('default', 'interpretationProtocols', 'basicMutesInterpretation', '{ score: 0, confidence: 0.10 }' );
+INSERT INTO protocolParameterSelections (userID, protocolCategoryTableName, protocolSlug, selectedParameters ) VALUES ('default', 'interpretationProtocols', 'basicReportsInterpretation', '{ score: 0, confidence: 0.20 }' );
+INSERT INTO protocolParameterSelections (userID, protocolCategoryTableName, protocolSlug, selectedParameters ) VALUES ('default', 'interpretationProtocols', 'expandedReportsInterpretation', '{ reportTypesGroupA: { reportTypes: [ malware, illegal, spam, impersonation ], score: 1, confidence: 0.5 }, reportTypesGroupB: { reportTypes: [ profanity, nudity ], score: 1, confidence: 0.02 }, reportTypesGroupC: { reportTypes: [ other ], score: 1, confidence: 0.1 }, }' );
+INSERT INTO protocolParameterSelections (userID, protocolCategoryTableName, protocolSlug, selectedParameters ) VALUES ('default', 'grapeRankProtocols', 'basicGrapevineNetwork', '{ attenuation: 0.8, rigor: 0.25, defaultUserScore: 0, defaultUserScoreConfidence: 0.01 }' );
 
 -- for new user Alice:
-INSERT INTO protocolParameterSelections (userPubkey, protocolCategoryTableName, protocolSlug, selectedParameters ) VALUES (<pk_Alice>, "interpretationProtocols", "basicFollowsInterpretation", "{ score: 1, confidence: 0.05 }" );
+INSERT INTO protocolParameterSelections (userPubkey, protocolCategoryTableName, protocolSlug, selectedParameters ) VALUES (<pk_Alice>, 'interpretationProtocols', 'basicFollowsInterpretation', '{ score: 1, confidence: 0.05 }' );
 
 -- when Alice updates her parameter selections:
 
 UPDATE protocolParameterSelections SET selectedParameters = newParams WHERE userID = AliceId AND protocolCategoryTableName = protocolCategoryTableName AND protocolSlug = protocolSlug
   
 /*
-"{ attenuation: 0.8, rigor: 0.25, defaultUserScore: 0, defaultUserScoreConfidence: 0.01 }"
-"{ defaultProductScore: 0, defaultProductScoreConfidence: 0.05 }"
+'{ attenuation: 0.8, rigor: 0.25, defaultUserScore: 0, defaultUserScoreConfidence: 0.01 }'
+'{ defaultProductScore: 0, defaultProductScoreConfidence: 0.05 }'
 */
 
 
@@ -143,7 +137,7 @@ REVIEW: coreTable4b and coreTable4c are completely deprecated and fully replaced
 
 -- coreTable4b
 CREATE TABLE IF NOT EXISTS defaultInterpretationProtocolSolutions(
-  ID INT PRIMARY KEY NOT NULL,
+  ID SERIAL PRIMARY KEY,
   interpretationProtocolId INT NOT NULL,
   slug TEXT NOT NULL,
   name TEXT NOT NULL,
@@ -151,14 +145,14 @@ CREATE TABLE IF NOT EXISTS defaultInterpretationProtocolSolutions(
   parameterSchema TEXT NOT NULL, -- naddr pointer to a JSON Schema which specifies the required parameters
 );
 
-INSERT INTO defaultInterpretationProtocolSolutions [(interpretationProtocolId, name)] VALUES (0, "Standard Brainstorm Follows Interpretation"); -- params: score = 1, confidence = 0.05
-INSERT INTO defaultInterpretationProtocolSolutions [(interpretationProtocolId, name)] VALUES (1, "Standard Brainstorm Mutes Interpretation"); -- params: score = 0, confidence = 0.1
-INSERT INTO defaultInterpretationProtocolSolutions [(interpretationProtocolId, name)] VALUES (2, "Standard Brainstorm Reports Interpretation"); -- params: score = 0, confidence = 0.2 regardless of reportType
-INSERT INTO defaultInterpretationProtocolSolutions [(interpretationProtocolId, name)] VALUES (3, "Alternate Brainstorm Reports Interpretation"); -- params: score = 0, confidence = 0.2 for all reportTypes except nudity, which is ignored
+INSERT INTO defaultInterpretationProtocolSolutions interpretationProtocolId, name) VALUES (0, 'Standard Brainstorm Follows Interpretation'); -- params: score = 1, confidence = 0.05
+INSERT INTO defaultInterpretationProtocolSolutions interpretationProtocolId, name) VALUES (1, 'Standard Brainstorm Mutes Interpretation'); -- params: score = 0, confidence = 0.1
+INSERT INTO defaultInterpretationProtocolSolutions interpretationProtocolId, name) VALUES (2, 'Standard Brainstorm Reports Interpretation'); -- params: score = 0, confidence = 0.2 regardless of reportType
+INSERT INTO defaultInterpretationProtocolSolutions interpretationProtocolId, name) VALUES (3, 'Alternate Brainstorm Reports Interpretation'); -- params: score = 0, confidence = 0.2 for all reportTypes except nudity, which is ignored
 
 -- table4c
 CREATE TABLE IF NOT EXISTS userInterpretationProtocolSolutions(
-  ID INT PRIMARY KEY NOT NULL,
+  ID SERIAL PRIMARY KEY,
   interpretationProtocolId INT NOT NULL,
   userId INT NOT NULL,
   slug TEXT NOT NULL,
@@ -167,8 +161,8 @@ CREATE TABLE IF NOT EXISTS userInterpretationProtocolSolutions(
   parameterSchema TEXT NOT NULL, -- naddr pointer to a JSON Schema which specifies the required parameters
 );
 
-INSERT INTO userInterpretationProtocolSolutions [(interpretationProtocolId, userId, name)] VALUES (0, AlicdId, "My Usual Brainstorm Follows Interpretation"); -- params: score = 1, confidence = 0.05
-INSERT INTO userInterpretationProtocolSolutions [(interpretationProtocolId, userId, name)] VALUES (0, AlicdId, "My Alternate Brainstorm Follows Interpretation"); -- params: score = 1, confidence = 0.08
+INSERT INTO userInterpretationProtocolSolutions interpretationProtocolId, userId, name) VALUES (0, AlicdId, 'My Usual Brainstorm Follows Interpretation'); -- params: score = 1, confidence = 0.05
+INSERT INTO userInterpretationProtocolSolutions interpretationProtocolId, userId, name) VALUES (0, AlicdId, 'My Alternate Brainstorm Follows Interpretation'); -- params: score = 1, confidence = 0.08
 
 
 
