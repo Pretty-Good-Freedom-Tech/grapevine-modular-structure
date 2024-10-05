4bad3593f828d056192ece21c9d88d3278f3592c
PrettyGoodFreedomTech <pretty.good.freedom.tech@gmail.com>
2024-10-04 21:12:12 -0400

    Update database-initialization-core.sql

--- interpretation-engine/nostr/database-initialization-core.sql
+++ interpretation-engine/nostr/database-initialization-core.sql
@@ -11,13 +11,12 @@ GrapevineNostrInterpretationEngine_core.db
 -- coreTable1
 CREATE TABLE IF NOT EXISTS interpretationProtocols(
   ID SERIAL PRIMARY KEY,
-  slug TEXT UNIQUE NOT NULL,
   universalInterpretationProtocolID TEXT NOT NULL, -- used to communicate with the nostr calculation engine; might be the same as the slug
   name TEXT, -- optional
   title TEXT, -- optional
-  description TEXT NOT NULL,
+  description TEXT,
   parametersJsonSchema JSONB, -- stringified json that describes the object that holds parameters that must be communicated across the API
-  // OPTIONAL: use naddr to point to the jsonSchema in place of the parametersJsonSchema column
+  -- OPTIONAL: use naddr to point to the jsonSchema in place of the parametersJsonSchema column
   parametersJsonSchemaNaddr TEXT, -- naddr that points to an event in which the json schema is stored (? stringified and placed in content; ? kind)
 );
 
